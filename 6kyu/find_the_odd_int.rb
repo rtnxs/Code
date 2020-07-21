79944a68dea8a52c0256866714b0f4f1ef175d33
rtnxs <riotnexus@mail.ru>
2020-07-21 15:36:08 +0300

    refactor find the oddit

--- 6kyu/find_the_odd_int.rb
+++ 6kyu/find_the_odd_int.rb
@@ -1,21 +1,20 @@
 def find_it(seq)
-  seq.find { |n| seq.count(n).odd? }   # аналог метода find метод detect
+  seq.find { |n| seq.count(n).odd? } # аналог метода find метод detect
 end
-p find_it(array=[20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
 
 # kata solution
 # def find_it(seq)
 #   seq.reduce(:^)
 # end
-# find_it(array=[20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
-
+#
+#
 # def find_it(seq)
 #   seq.uniq.each do |val|
 #     return val if seq.count(val).odd?
 #   end
 # end
-# find_it(array=[20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
-
+#
+#
 # def find_it(seq)
 #   seq.each do |i|
 #     if seq.count(i) % 2 != 0
@@ -23,9 +22,8 @@ p find_it(array=[20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
 #     end
 #   end
 # end
-# find_it(array=[20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
-
+#
+#
 # def find_it(seq)
 #   seq.uniq.each{|x| return x if seq.count(x).odd?}
 # end
-# find_it(array=[20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
